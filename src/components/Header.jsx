5e6eec1d76d9ccbc64ebb7185cbd8f77b2685489
Najaf Ali <najaf.balti000@gmail.com>
2024-03-23 00:13:54 +0500

    First

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <main className="flex items-center justify-between w-[90%] py-7">
+            <Link href="/">
+                <p>StirllingPhotography</p>
+            </Link>
+            <ul className="header-menu-list flex gap-4 items-center justify-center">
+                <Link href="/services">
+                    <li className="header-menu poppins-font">About</li>
+                </Link>
+                <Link href="/services">
+                    <li className="header-menu poppins-font">Portfolio</li>
+                </Link>
+                <Link href="/services">
+                    <li className="header-menu poppins-font">Services</li>
+                </Link>
+                <Link href="/contact">
+                    <li className="header-menu poppins-font">Contact</li>
+                </Link>
+            </ul>
+            <Link
+                href="/contact"
+                className="px-9 bg-white rounded-full text-black py-2"
+            >
+                Contact Us
+            </Link>
+        </main>
+    )
+}
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
+
+export const HeroSection = () => {
+    return (
+        <section className='w-[90%] flex justify-between'>
+            <div className='py-48 flex flex-col gap-8 w-[70%] items-center'>
+                <div className='flex flex-col gap-4 items-center'>
+                    <h1 className='regular-48'>Stirling Grady Photography</h1>
+                    <hr className='w-1/2 h-[1px] bg-white' />
+                    <p>Professional photographer traveling the United States.</p>
+                </div>
+                <Link href="/" className='px-20 py-4 border-[1px] border-white'>Book Now</Link>
+            </div>
+            <div className='flex flex-col items-center gap-8'>
+                <hr className='w-[2px] h-[50%] bg-white' />
+                <Link href="/">
+                    <FaFacebookF className='text-2xl' />
+                </Link>
+                <Link href="/">
+                    <FaInstagram className='text-2xl' />
+                </Link>
+                <Link href="/">
+                    <FaTwitter className='text-2xl' />
+                </Link>
+                <hr className='w-[2px] h-[50%] bg-white' />
+            </div>
+        </section>
+    )
+}
